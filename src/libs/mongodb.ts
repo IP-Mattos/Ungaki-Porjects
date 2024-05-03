26d4fd9bd347e08c4f0f69edcf6e2064b6b42908
unknown <ipmattoscontactos@gmail.com>
2024-05-03 17:23:54 -0300

    chore: change URL to connect mongodb

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
 
-const MONGO_URL = 'mongodb+srv://demon:demon@cluster0.uwon7ln.mongodb.net/'
+const MONGO_URL = 'mongodb+srv://guillermonicrosi:zach12345@cluster0.1aiue4q.mongodb.net/ungaki'
 
 export const connectMongoDB = async () => {
   try {
     await mongoose.connect(MONGO_URL)
+
     console.log('Conectado')
   } catch (error) {
     console.log(error)
